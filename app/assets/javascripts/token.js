e419e21e5f0dcdd61241d62bf531e30f4978fe07
Jean-Philippe Bougie <jp.bougie@gmail.com>
2012-12-01 16:53:06 -0500

    good token

--- app/assets/javascripts/token.js
+++ app/assets/javascripts/token.js
@@ -7,8 +7,8 @@ var domain = location.hostname;
 
 // Token de développement localhost
 if (domain == 'localhost')
-	var playback_token = "GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=";
+  window.playback_token = "GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=";
 // Token authentifié
 else
-	var playback_token = "GABQunkY_____3o3eXd2cng5MnNqOW1lenByam55ejNhaIhdYCWF6dN36iT9xZ54R78=";
+	window.playback_token = "GAxQuns9_____3o3eXd2cng5MnNqOW1lenByam55ejNhaGpwYm91Z2llLmNvbTRd2xNVKsBMP9CrzISZp00=";
 
