fbd55542ccb1a4ec506bdfb8726c97f8472a8c91
Jean-Philippe Bougie <jp.bougie@gmail.com>
2012-12-01 16:40:39 -0500

    new token

--- app/assets/javascripts/token.js
+++ app/assets/javascripts/token.js
@@ -10,5 +10,5 @@ if (domain == 'localhost')
 	var playback_token = "GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=";
 // Token authentifié
 else
-	var playback_token = "GABQukMn_____3o3eXd2cng5MnNqOW1lenByam55ejNhaKQ1mwSFmKnsoqikOywyDyE=";
+	var playback_token = "GABQunkY_____3o3eXd2cng5MnNqOW1lenByam55ejNhaIhdYCWF6dN36iT9xZ54R78=";
 
