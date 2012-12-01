6c7f84351d4b7cc3365570c07e343b9965428398
Francois Bernier <frankbernier@gmail.com>
2012-12-01 11:19:56 -0500

    Initial commit

---
+++ config/initializers/rdio.rb
@@ -0,0 +1,1 @@
+RDIO = RdioApi.new(:consumer_key => 'z7ywvrx92sj9mezprjnyz3ah', :consumer_secret => 'eeqAUjjfPm')
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackqc::Application.config.secret_token = 'c447f05949088183a23513c64a90b97f321d5be80cdb6e3440accd8b3f6eda93c67dbe1c10c51e791975b1a85fb26ad5c0467208775c8be64705b9b7e679c942'
