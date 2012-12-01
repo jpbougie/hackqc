c74f484265b27d54e5021b9d0365ac566fbd9d67
Francois Bernier <frankbernier@gmail.com>
2012-12-01 18:27:59 -0500

    OAUTH MOFOCKA

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   layout "application"
   def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
-    self.current_user = @user
+    @user = User.find_or_create_from(:auth_hash => auth_hash)
+    session[:current_user] = @user
     redirect_to '/'
   end
 
